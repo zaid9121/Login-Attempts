fbf93bd76e341a691c06e1d6a1f367c85d8b0f53
zaid <alexsanderson610@gmail.com>
2024-03-17 11:01:27 +0100

    fixed

--- dashboard.php
+++ dashboard.php
@@ -1,14 +1,12 @@
 <?php
-
 session_start();
-$conn = mysqli_connect("localhost","root","","tutorials");
+$conn = mysqli_connect("localhost", "root", "", "tutorials");
 
-if(!isset($_SESSION['USER_ID'])) {
-   header("location:login_form.php");
-   die();
+if (!isset($_SESSION['USER_ID'])) {
+    header("location:login_form.php");
+    die();
     echo "<script> location.replace('login_form.php') </script>";
 }
-
 
 ?>
 <h1><?php echo "Welcome" . $_SESSION['USER_ID']; ?></h1>
